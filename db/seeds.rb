74a44adbba6b0c1e372b8bd3fd7cda57209fa3d5
yusuke ishida <ishidayusuke01@gmail.com>
2020-05-14 14:20:27 +0900

    Seeded the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Getting the API for the ingredients'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+json_serialized = open(url).read
+ingredients_json = JSON.parse(json_serialized)
+# puts json_serialized
+ingredients = ingredients_json['drinks']
+
+# puts ingredients
+# ingredients.each do |ingredient|
+#   puts ingredient["strIngredient1"]
+# end
+# puts 'Creating ingredients'
+
+ingredients.each do |ingredient|
+  Ingredient.create(
+    name: ingredient['strIngredient1']
+  )
+end
+
+puts "Created #{Ingredient.count} ingredients"
